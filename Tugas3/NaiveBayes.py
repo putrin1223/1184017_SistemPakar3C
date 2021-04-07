ef4daacb558f10ddb7ff726a3ce8a615613346af
putrin1223 <56755972+putrin1223@users.noreply.github.com>
2021-04-07 20:04:53 +0700

    Add files via upload

---
+++ Tugas3/NaiveBayes.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+import numpy as np
+import math
+from scipy.sparse import *
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+from pandas import DataFrame
+
+trainData = pd.read_csv('train.data', sep=' ',header=None,names=['docId','wordId','count'])
+trainLabels = pd.read_csv('train.label')
+
+testData = pd.read_csv('test.data', sep=' ',header=None,names=['docId','wordId','count'])
+testLabels = pd.read_csv('test.label')
+
+
+
+df = pd.DataFrame(data=0, index = np.arange(11268), columns = np.arange(53976))
+
+length=0
+inner=0
+index=0
+Doclength = len(set(trainData.wordId))
+for i in range (11267):
+    df.set_value([trainData.docId[i]], [trainData.wordId[i]], 1)  
+    
+
+model = DecisionTreeClassifier()
+model.fit(df,trainLabels)
+print("Model is -")
+print("Prediction is - ")
+
+testData = testData[(testData.wordId.isin(trainData.wordId))]
+trainData=None
+trainLabels=None
+testLabels=None
+df=None
+del index
+del length
+del testLabels
+ef=pd.DataFrame(data=0, index = np.arange(11268), columns = np.arange(53976))
+
+
+for j in range(11267):
+    ef.set_value([j],[testData.wordId[j]],1)
+from sklearn.naive_bayes import BernoulliNB
+bernaulli = BernoulliNB()
+bernaulli.fit(df, trainLabels)
+BernoulliNB(alpha=0.0, binarize=0.0, class_prior=None, fit_prior=True)
+print(clf.predict(ef))
